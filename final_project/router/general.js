8a41567857ee011d4b1c78f95fd0d5b23c3f0096
muhddanishaiman <112624478+muhddanishaiman@users.noreply.github.com>
2025-09-20 11:43:29 -0400

    Updated general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -33,13 +33,14 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-    
+
+    res.status();
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    
+    res.status();
 });
 
 //  Get book review
