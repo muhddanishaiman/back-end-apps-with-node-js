02b2ce367e6c8f005749d9ff729c3a537de44fe4
muhddanishaiman <112624478+muhddanishaiman@users.noreply.github.com>
2025-09-20 11:42:13 -0400

    uploaded project

---
+++ final_project/router/general.js
@@ -0,0 +1,51 @@
+const express = require('express');
+let books = require("./booksdb.js");
+let isValid = require("./auth_users.js").isValid;
+let users = require("./auth_users.js").users;
+const public_users = express.Router();
+
+
+public_users.post("/register", (req,res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (username && password) {
+        if (!isValid(username)) {
+            users.push({"username": username, "password": password});
+            return res.status(200).json({message: "Customer successfully registered. Now you can login"});
+        } else {
+            return res.status(404).json({message: "Customer already exists!"});
+        }
+    }
+    return res.status(404).json({message: "Unable to register customer."});
+});
+
+// Get the book list available in the shop
+public_users.get('/',function (req, res) {
+    res.send(JSON.stringify(books, null, 4));
+});
+
+// Get book details based on ISBN
+public_users.get('/isbn/:isbn',function (req, res) {
+    const isbn = req.params.isbn;
+    res.send(books[isbn]);
+});
+  
+// Get book details based on author
+public_users.get('/author/:author',function (req, res) {
+    
+});
+
+// Get all books based on title
+public_users.get('/title/:title',function (req, res) {
+  //Write your code here
+  return res.status(300).json({message: "Yet to be implemented"});
+});
+
+//  Get book review
+public_users.get('/review/:isbn',function (req, res) {
+    const isbn = req.params.isbn;
+    res.send(books[isbn]["reviews"]);
+});
+
+module.exports.general = public_users;
